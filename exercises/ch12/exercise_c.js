5212d05a579e6eea9baf5c75e85b6562ffa2802e
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-08-10 22:24:02 +0200

    Finished ch12 exercises

--- exercises/ch12/exercise_c.js
+++ exercises/ch12/exercise_c.js
@@ -6,4 +6,14 @@
 // Use traversable to rearrange and flatten the nested Tasks & Maybe
 
 // readFirst :: String -> Task Error (Maybe (Task Error String))
-const readFirst = compose(map(map(readfile('utf-8'))), map(safeHead), readdir);
+
+
+// const readFirst = compose(map(map(readfile('utf-8'))), map(safeHead), readdir);
+
+const trace = curry((tag, x) => {
+    console.log(tag, x);
+    return x;
+  });
+
+  
+const readFirst = compose(chain(traverse(Task.of, readfile('utf-8'))), map(safeHead), readdir);
